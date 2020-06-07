fef6a9a7182586ff47689956d3f697eb1cca9426
wahaj ahmed iqbal <wahaj465@gmail.com>
2020-06-07 17:47:43 +0500

    6th

---
+++ home.js
@@ -0,0 +1,34 @@
+console.log("#################### 1 #############");
+// function fullname( frst , lst)
+//     {
+//         return ("hello " + frst +" "+ lst);
+//     }
+// var frst = prompt("Enter First Name : ");
+// var lst = prompt("Enter Last Name : ");
+// console.log(fullname(frst,lst))
+console.log("#################### 2 #############");
+// function lngthfun(vari)
+//     {
+//         var fndlnght = phn.length;
+//         return fndlnght;
+//     }
+// var phn = prompt("Enter you're phone model : ");
+// console.log(lngthfun(phn))
+console.log("#################### 3 #############");
+// function indx() {
+//     var indxinput = "Pakistannnn";
+//     return indxinput.indexOf("n")
+// }
+// var print = indx();
+// console.log(print)
+console.log("#################### 4 #############");
+// var wrd = "Hello world";
+// var  n = wrd.lastIndexOf("l");
+// console.log(n)
+console.log("#################### 5 #############");
+// var word5 = "Pakistan";
+// console.log(word5.charAt(3));
+console.log("#################### 4 #############");
+// var frst = prompt("Enter First Name : ");
+// var lst = prompt("Enter Last Name : ");
+// console.log(frst.concat(" "+lst));
