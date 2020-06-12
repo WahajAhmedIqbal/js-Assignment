865e39adc3fd66ebb4a9dc4808898796a04bc29c
wahaj ahmed iqbal <wahaj465@gmail.com>
2020-06-12 23:18:32 +0500

    last comit

--- home.js
+++ home.js
@@ -28,7 +28,118 @@ console.log("#################### 4 #############");
 console.log("#################### 5 #############");
 // var word5 = "Pakistan";
 // console.log(word5.charAt(3));
-console.log("#################### 4 #############");
+console.log("#################### 6 #############");
 // var frst = prompt("Enter First Name : ");
 // var lst = prompt("Enter Last Name : ");
 // console.log(frst.concat(" "+lst));
+console.log("#################### 7 #############");
+// var replaceword = "Hyderabad";
+// var wordreplace = replaceword.replace(/Hyder/gi,"Islam");
+// console.log(wordreplace)
+console.log("#################### 8 #############");
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var replacemsg = message.replace(/and/gi,"&");
+// console.log(replacemsg)
+console.log("#################### 9 #############");
+// var numInput = parseInt(prompt("Enter number :"));
+// console.log("Type of " + typeof(numInput) + " Enter number " + numInput );
+console.log("#################### 10 #############");
+// var domaininput = prompt("Enter Domain : ");
+// var extrctdomin = domaininput.substring(4);
+// console.log("Domain Name "+extrctdomin);
+console.log("#################### 11 #############");
+// var uperword = prompt("Enter any word uh want to capatilize ");
+// var uperstr = uperword.toUpperCase();
+// console.log(uperstr);
+console.log("#################### 12 #############");
+// var uperword = prompt("Enter any word uh want to lowercase ");
+// var uperstr = uperword.toLowerCase();
+// console.log(uperstr);
+console.log("#################### 13 #############");
+// var a = "3";
+// var b = "3";
+// if (a === b)
+//     {
+//         console.log("x = " + a + b );
+//     }
+// else
+//     {
+//         console.log("not same");
+//     }
+console.log("#################### 14 #############");
+// var a = "3";
+// var b = "3";
+// if (a === b)
+//     {
+//         console.log("y = " + (a - b) );
+//     }
+// else
+//     {
+//         console.log("not same");
+//     }
+console.log("#################### 15 #############");
+// var deciNum = 35.36;
+// var replacenum = deciNum + "";
+// replacenum = replacenum.replace(".","");
+// replacenum = parseInt(replacenum);
+// alert(replacenum)
+console.log("#################### 16 #############");
+// var lst =["cake", "apple pie", "cookie", "chips", "patties"];
+// var sweetinput = prompt("Enter cake or whatever ");
+// var sweetsmal = sweetinput.toLowerCase();
+// if(lst.includes(sweetsmal))
+//     {
+//         console.log("yup avalible :" + sweetsmal);
+//     } 
+// else
+//     {
+//         console.log("Opps sorry !");
+//     }
+console.log("#################### 17 #############");
+// var input1 = prompt("Enter para : ");
+// var input2 = prompt("Enter para : ");
+// if (input1.length > input2.length)
+//     {
+//         console.log("input 1 is grater ");
+//     }
+// else if (input1.length < input2.length)
+//     {
+//         console.log("input 2 is grater ");
+//     }
+// else if (input1.length == input2.length)
+//     {
+//         console.log("inputs are equal ");
+//     }
+console.log("#################### 18 #############");
+// var lastinput = prompt("Enter name : ");
+// var lstinput = lastinput[lastinput.length  -1];
+// console.log( lstinput)
+console.log("#################### 19 #############");
+// var para = "The quick brown fox the jumps over the lazy dog";
+// var matchWord = para.toLowerCase();
+// var mw = /the/gi;
+// var result = para.match(mw);
+// var resultCount = 0;
+// if(Array.isArray(result))   
+//     {
+//         for( i = 0; i < result.length; i++)
+//             {
+//                 resultCount++;
+//             }
+//             console.log("Count is " + resultCount)
+//     }
+console.log("#################### 20 ############# ask quesstion"); 
+var university = "University of Karachi";
+var uni = university.split("");
+document.write(uni);
+console.log("#################### 21 ############# "); 
+// var word = prompt("ENter user name :");
+// var wrd = word.toLowerCase();
+// var check = /[@.,!]/
+// if(check.test(wrd))
+//     {
+//         console.log("Please enter a valid user name");
+//     }
+// else{
+//     console.log("Welcome to karachi !")
+// }
